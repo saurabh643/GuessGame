cfe5d2beb0820e8bcc4a61b8026f5efa5f4b4d83
SaurabhLohani <68461646+saurabh643@users.noreply.github.com>
2020-07-25 15:48:48 +0530

    Add files via upload

---
+++ guess2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 14:00:14 2020
+
+@author: Saurabh
+"""
+
+
+n=18
+number_of_guesses=1
+print("Number of guesses is limited to only 9 times: ")
+while (number_of_guesses<=9):
+    guess_number = int(input("Guess the number :\n"))
+    if guess_number<18:
+        print("you enter less number please input greater number.\n")
+    elif guess_number>18:
+        print("you enter greater number please input smaller number.\n ")
+    else:
+        print("you won\n")
+        print(number_of_guesses,"no.of guesses he took to finish.")
+    break
+    print(9-number_of_guesses,"no. of guesses left")
+number_of_guesses = number_of_guesses + 1
+
+if(number_of_guesses>=9):
+    print("Game Over")
+  
